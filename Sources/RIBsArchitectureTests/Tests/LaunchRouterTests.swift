22d1e0fed24ebf1fa6028f20e6651e2f91d9e5d8
SungJun Hong <castle_jun@daum.net>
2026-03-11 11:17:38 +0900

    Add LaunchRouterTests

---
+++ Sources/RIBsArchitectureTests/Tests/LaunchRouterTests.swift
@@ -0,0 +1,27 @@
+@testable import RIBsArchitecture
+
+import UIKit
+import Testing
+
+struct LaunchRouterTests {
+  @Test
+  @MainActor
+  func testLaunchFromWindow() {
+    let interactor = InteractorMock()
+    let viewController = ViewControllableMock()
+    let router = LaunchRouter(interactor: interactor, viewController: viewController)
+    let window = WindowMock(frame: .zero)
+    
+    #expect(window.rootViewController == nil)
+    #expect(window.isKeyWindow == false)
+    #expect(window.makeKeyAndVisibleCallCount == 0)
+    #expect(interactor.activateCallCount == 0)
+    
+    router.launch(from: window)
+    
+    #expect(window.rootViewController == viewController.uiViewController)
+    #expect(window.isKeyWindow)
+    #expect(window.makeKeyAndVisibleCallCount == 1)
+    #expect(interactor.activateCallCount == 1)
+  }
+}
